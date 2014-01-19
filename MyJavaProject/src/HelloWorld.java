9f4d8a34b9c1ba91596561e314d5229592ddea5b
Zia khalid <zia.khalid@webshrub.com>
2014-01-19 13:45:07 +0530

    zia idea changes

--- MyJavaProject/src/HelloWorld.java
+++ MyJavaProject/src/HelloWorld.java
@@ -2,7 +2,11 @@
 public class HelloWorld {
 	public static void main(String[]args){
 		System.out.println("Welcome to My World");
-		System.out.println("yo man love this ");
+		System.out.println("yo man love this2 ");
+		System.out.println("yo man love this 3");
+		System.out.println("yo man love this 4");
+		System.out.println("yo man love this 5")
+        ;
 	}
 
 }
